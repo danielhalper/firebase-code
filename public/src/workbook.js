5ae20d4bc776e304426f92707dcc5928fc364965
Arlette Pineda <arlettepimo@gmail.com>
2021-06-28 17:27:05 -0700

    Added forms and css to align content.

--- public/src/workbook.js
+++ public/src/workbook.js
@@ -5,30 +5,24 @@ const { RightOutlined } = icons;
 class Workbook extends React.Component {
 
   render() {
-    // no scroll
-    //class="select-item-box select-item " use display:none to hide duplicate info
-    //center form
     return (
-      <div>
-        <h1>Training Modules</h1>
-        <h3>Approximate time: 1-2 hours</h3>
-        <p>Please take your time when completing the Step Up workbook. It is essential for....</p>
-        <div>
-          <Tabs defaultActiveKey="1" centered>
-            <TabPane tab="Program Summary" key="1">
-              Content of Tab Pane 1
+      <div className="section-content" id="workbook-section-content">
+        <h1 className="section-header-h1">Training Modules</h1>
+        <h3 className="section-approx-time">Approximate time: 1-2 hours</h3>
+        <p className="section-p">Please take your time when completing the Step Up workbook. It is essential for....</p>
+        <div className="workbook-tab-style">
+          <Tabs defaultActiveKey="1" centered className="tab-content-style">
+            <TabPane tab="General Expectations" key="1" forceRender="true">
+              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScN2qPXuWx7PcG4rAPzFUx06M-V5Ahtx0o-ge1L3ifc-9VVFQ/viewform?embedded=true" width="640" height="1417" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
             </TabPane>
-            <TabPane tab="General Expectations" key="2">
-              Content of Tab Pane 2
+            <TabPane tab="Sexual Harassment and Boundaries" key="2" forceRender="true">
+              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdx3uxtfVJEf8jSs4bXwKUFZp0a5teFBxxs-vwod3koxJ1gbA/viewform?embedded=true" width="640" height="1417" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
             </TabPane>
-            <TabPane tab="Sexual Harassment and Boundaries" key="3">
-              Content of Tab Pane 3
+            <TabPane tab="Teaching Math" key="3" forceRender="true">
+              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeG5hnnpsDSCjelPD31tm3q8jIxd_AqiphhUpicC_DPvJSZCA/viewform?embedded=true" width="640" height="1417" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
             </TabPane>
-            <TabPane tab="Teaching Math" key="4">
-              Content of Tab Pane 4
-            </TabPane>
-            <TabPane tab="Teaching Reading" key="5">
-              Content of Tab Pane 5
+            <TabPane tab="Teaching Reading" key="4" forceRender="true">
+              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScewxx296TOYqKFJEPDKKzPi56A-bvKdBoZG3OtDhzAnkMNZQ/viewform?embedded=true" width="640" height="1417" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
             </TabPane>
           </Tabs>
         </div>
