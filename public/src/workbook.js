4ce21bf7c058edcfc1545472c73d5012fd56058e
Arlette Pineda <arlettepimo@gmail.com>
2021-06-30 16:54:38 -0700

    Changed height of google forms to remove some inner scrolls.

--- public/src/workbook.js
+++ public/src/workbook.js
@@ -13,16 +13,16 @@ class Workbook extends React.Component {
         <div className="workbook-tab-style">
           <Tabs defaultActiveKey="1" centered className="tab-content-style">
             <TabPane tab="General Expectations" key="1" forceRender="true">
-              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScN2qPXuWx7PcG4rAPzFUx06M-V5Ahtx0o-ge1L3ifc-9VVFQ/viewform?embedded=true" width="640" height="1417" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScN2qPXuWx7PcG4rAPzFUx06M-V5Ahtx0o-ge1L3ifc-9VVFQ/viewform?embedded=true" width="640" height="3200" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
             </TabPane>
             <TabPane tab="Sexual Harassment and Boundaries" key="2" forceRender="true">
-              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdx3uxtfVJEf8jSs4bXwKUFZp0a5teFBxxs-vwod3koxJ1gbA/viewform?embedded=true" width="640" height="1417" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdx3uxtfVJEf8jSs4bXwKUFZp0a5teFBxxs-vwod3koxJ1gbA/viewform?embedded=true" width="640" height="2100" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
             </TabPane>
             <TabPane tab="Teaching Math" key="3" forceRender="true">
-              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeG5hnnpsDSCjelPD31tm3q8jIxd_AqiphhUpicC_DPvJSZCA/viewform?embedded=true" width="640" height="1417" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeG5hnnpsDSCjelPD31tm3q8jIxd_AqiphhUpicC_DPvJSZCA/viewform?embedded=true" width="640" height="2400" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
             </TabPane>
             <TabPane tab="Teaching Reading" key="4" forceRender="true">
-              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScewxx296TOYqKFJEPDKKzPi56A-bvKdBoZG3OtDhzAnkMNZQ/viewform?embedded=true" width="640" height="1417" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScewxx296TOYqKFJEPDKKzPi56A-bvKdBoZG3OtDhzAnkMNZQ/viewform?embedded=true" width="640" height="1550" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
             </TabPane>
           </Tabs>
         </div>
