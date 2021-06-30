ee782610e214afd69184e4ae843bc8a4e6d8324d
Arlette Pineda <arlettepimo@gmail.com>
2021-06-30 16:57:20 -0700

    Removed unused code.

--- public/src/chat-signup.js
+++ public/src/chat-signup.js
@@ -3,24 +3,18 @@ const { Row, Col } = antd;
 class ChatSignup extends React.Component {
 
   render() {
-    // no scroll
-    //class="select-item-box select-item " use display:none to hide duplicate info
-    //center form
-   return (
+  return (
     <div>
-      <h1>15-Minute Chat</h1>
+      <h1 className="section-header-h1">15-Minute Chat</h1>
 
-      <p>We want to keep it casual, ask a few questions, and get to know you better!</p>
+      <p className="section-p">We want to keep it casual, ask a few questions, and get to know you better!</p>
 
-      <p>All meetings with us are via Zoom, be on the lookout for the link in your confirmation email.</p>
+      <p className="section-p">All meetings with us are via Zoom, be on the lookout for the link in your confirmation email.</p>
 
-      <p>If you need assistance, please reach out to our Onboarding Specialist at laura@stepuptutoring.org or (205) 953-1894. If you have already signed up or completed session, please disregard.</p>
-
-      <Row>
-        <Col  xs={24}>
-          <iframe  src="https://app.acuityscheduling.com/schedule.php?owner=21394641&appointmentType=19663042" title="Schedule Appointment" width="90%" height="800" frameBorder="0"></iframe><script src="https://embed.acuityscheduling.com/js/embed.js" type="text/javascript"></script>
-        </Col>
-      </Row>
+      <p className="section-p">If you need assistance, please reach out to our Onboarding Specialist at laura@stepuptutoring.org or (205) 953-1894. If you have already signed up or completed session, please disregard.</p>
+      <div className="center-embed-chat-signup">
+        <iframe src="https://app.acuityscheduling.com/schedule.php?owner=21394641&appointmentType=18777910" title="Schedule Appointment" width="90%" height="1200" frameBorder="0"></iframe><script src="https://embed.acuityscheduling.com/js/embed.js" type="text/javascript"></script>
+      </div>
     </div>
     )
   }
