fcbac13b37bf98f0ba2aaea7fb41d58ed92ad1e3
Arlette Pineda <arlettepimo@gmail.com>
2021-07-02 11:29:26 -0700

    Successfully prefilled waiver form.

--- public/src/waiver.js
+++ public/src/waiver.js
@@ -1,21 +1,21 @@
-const { Row, Col } = antd;
-
 class Waiver extends React.Component {
 
   componentDidMount() {
-  const script = document.createElement("script");
-  script.src = "https://paperform.co/__embed.min.js";
-  document.body.appendChild(script);
+    const script = document.createElement("script");
+    script.src = "https://paperform.co/__embed.min.js";
+    document.body.appendChild(script);
 };
 
   render() {
-
+    const userLocalEmail = getEmailFromLocalStorage();
+    const userLocalFirstName = getFirstNameFromLocalStorage();
+    const userLocalLastName = getLastNameFromLocalStorage();
     return (
       <div>
         <h1 className="section-header-h1">Waiver</h1>
         <h3 className="section-approx-time">Approximate time: 5 minutes</h3>
         <p className="section-p">The tutor waiver is a binding legal agreement between you (the tutor) and StepUp Tutoring. It outlines the tutor code of conduct, and responsibilities & expectations for our volunteer tutors. </p>
-            <div className="waiver-paperform-div" data-paperform-id="tqp1uzj8"></div>
+        <div className="waiver-paperform-div" data-prefill={`firstName=${userLocalFirstName}&lastName=${userLocalLastName}&email=${userLocalEmail}&2n0v7=${userLocalFirstName}%20${userLocalLastName}`} data-paperform-id="tqp1uzj8"></div>
       </div>
     )
   }
