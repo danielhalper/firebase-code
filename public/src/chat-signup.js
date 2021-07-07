ef140b72572f841a38e1f7ca264976df0cc56073
Arlette Pineda <arlettepimo@gmail.com>
2021-07-06 22:30:58 -0700

    Added classes for page design.

--- public/src/chat-signup.js
+++ public/src/chat-signup.js
@@ -29,7 +29,7 @@ class ChatSignup extends React.Component {
       <p className="section-p">All meetings with us are via Zoom, be on the lookout for the link in your confirmation email.</p>
 
       <p className="section-p">If you need assistance, please reach out to our Onboarding Specialist at laura@stepuptutoring.org or (205) 953-1894. If you have already signed up or completed session, please disregard.</p>
-      <div className="center-embed-chat-signup">
+      <div className="center-embed-iframe">
         <iframe id="my-deferred-iframe" src="about:blank" /><script src="https://embed.acuityscheduling.com/js/embed.js" type="text/javascript"></script>
       </div>
     </div>
