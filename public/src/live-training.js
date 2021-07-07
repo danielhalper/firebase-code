eede9a6486c9cf4c01cc4b0a84e6d8d0d2cf746b
Arlette Pineda <arlettepimo@gmail.com>
2021-07-06 19:45:41 -0700

    Created new file for live training content page.

---
+++ public/src/live-training.js
@@ -0,0 +1,11 @@
+
+class LiveTraining extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1 className="section-header-h1">Tell Me This Works</h1>
+      </div>
+    )
+  }
+}
