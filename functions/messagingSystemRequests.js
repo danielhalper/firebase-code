d0f4556332273ec47e819eb2966c43463e59167d
Caleb Hester <naclcaleb@gmail.com>
2021-07-08 20:48:10 -0700

    Get data syncing for all tutors, not just matched ones

--- functions/messagingSystemRequests.js
+++ functions/messagingSystemRequests.js
@@ -596,8 +596,7 @@ async function updatePeopleData() {
     //Get the tutor data from AirTable
     await base('Tutors').select({
         fields: ['First Name', 'Last Name', 'Email', 'Phone', 'Students', 'Tutor ID', 'Match date', 'StepUp Email'],
-        offset: 0,
-        view: 'Matched Tutors'
+        offset: 0
     }).eachPage((records, fetchNextPage) => {
         let _records = records
 
@@ -668,7 +667,6 @@ async function updatePeopleData() {
     return await base('Students').select({
         fields: ['Student ID', 'First Name', 'Last Name', "Guardian's Email", "Guardian's Phone", 'Tutors', 'Language'],
         offset: 0,
-        view: 'Matched Students'
     }).eachPage((records, fetchNextPage) => {
 
         let _records = records
