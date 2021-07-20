5b3596ccdc74ec83d4b5b61ede8830de50787f5c
Arlette Pineda <arlettepimo@gmail.com>
2021-07-20 13:52:14 -0700

    Increased iframe width.

--- public/src/live-training.js
+++ public/src/live-training.js
@@ -7,7 +7,7 @@ class LiveTraining extends React.Component {
     const iframe = document.getElementById("deferred-iframe");
     iframe.src = `https://calendly.com/stepup-tut/training?name=${userLocalFirstName}%20${userLocalLastName}&email=${userLocalEmail}`;
     iframe.title = "Schedule Live Training";
-    iframe.width = "90%";
+    iframe.width = "100%";
     iframe.height = "800";
     iframe.frameBorder = "0";
   };
@@ -21,7 +21,7 @@ class LiveTraining extends React.Component {
       <div>
         <h1 className="section-header-h1">Live Training</h1>
         <p>Attend an hour long, live virtual session with one of our coaches where you will find out more about how being a tutor with Step Up works, learn about some fun educational resources to use with your student, and ask any questions you may have. Please sign up for a session with Laura through the calendar below.</p>
-        <div className="center-embed-iframe" id="live-training-calendly">
+        <div id="live-training-calendly">
           <iframe id="deferred-iframe" src="about:blank" /><script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" ></script>
         </div>
       </div>
