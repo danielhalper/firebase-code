e4dec2c96eb9cdfe4153b4623c2e8daac39dd3b3
Caleb Hester <naclcaleb@gmail.com>
2021-07-31 12:29:24 -0700

    Move index.css out of dist

--- public/dist/onboarding-index.js
+++
@@ -1,8 +0,0 @@
-//The node to mount on
-var mountNode = document.getElementById('content');
-
-var EMULATOR = window.location.href.includes('localhost');
-
-if (EMULATOR) firebase.functions().useEmulator("localhost", 5001);
-
-ReactDOM.render(React.createElement(OnboardingApp, null), mountNode);
