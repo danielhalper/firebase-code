972c96e8de94fb7e4c2586a20b3cace7bd55746c
Arlette Pineda <arlettepimo@gmail.com>
2021-10-18 16:23:41 -0700

    Changed interview page logic to keep acuity confirmation page when user initially confirms one.

--- public/src/onboarding-content-pages/chat-signup.js
+++ public/src/onboarding-content-pages/chat-signup.js
@@ -7,23 +7,15 @@ class ChatSignup extends React.Component {
     let tutorDetails = this.props.tutorDetails;
     let interviewDate = '';
     if (notNull(interviewDate)) interviewDate = tutorDetails.interviewDate;
+    let iframeExists = false;
 
-    if (!this.props.progress.hasScheduledChat){
-      return (
-        <div>
-          <h1 className="section-header-h1">20-Minute Interview</h1>
+    if (document.getElementById('my-deferred-iframe')) {
+      iframeExists = true;
+    }
 
-          <p className="section-p">We want to keep it casual, ask a few questions, and get to know you better!</p>
+    // if (!this.props.progress.hasScheduledChat){
 
-          <p className="section-p">All meetings with us are via Zoom, be on the lookout for the link in your confirmation email.</p>
-
-          <p className="section-p">If you need assistance, please reach out to our Onboarding Manager at <strong><Link href='mailto:charmaine@stepuptutoring.org'>charmaine@stepuptutoring.org.</Link></strong></p>
-          <div className="center-embed-iframe">
-            <iframe id="my-deferred-iframe" title="Schedule Appointment" width="90%" height="600" frameBorder="0" src={`https://app.acuityscheduling.com/schedule.php?owner=21394641&appointmentType=18777910&firstName=${this.props.tutorDetails.firstname}&lastName=${this.props.tutorDetails.lastname}&email=${this.props.tutorDetails.email}`} />
-          </div>
-        </div>
-        )
-    } else if (this.props.progress.hasScheduledChat && tutorDetails.status === ''){
+    if (this.props.progress.hasScheduledChat && tutorDetails.status === '' && !iframeExists){
           return (
             <div>
               <h1 className = "section-header-h1" > 20-Minute Interview</h1 >
@@ -39,7 +31,7 @@ class ChatSignup extends React.Component {
               </div>
             </div>
           )
-    } else {
+    } else if (tutorDetails.status === 'Application Accepted') {
       return(
         <div>
           <h1 className="section-header-h1" > 20-Minute Interview {tutorDetails.status === 'Application Accepted' ? <span className="header-completed-done-check">< CheckOutlined /></span> : null}</h1 >
@@ -47,6 +39,21 @@ class ChatSignup extends React.Component {
             <img className="svg-completed-img" src="./meeting.svg" alt="calendar booked image"></img>
             <p>Thank you for completing the 20 minute interview with us, it was great to meet you {tutorDetails.firstname}.</p>
             <p>You're now one step closer to becoming a Step Up Tutor and making a difference in a child's life!</p>
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h1 className="section-header-h1">20-Minute Interview</h1>
+
+          <p className="section-p">We want to keep it casual, ask a few questions, and get to know you better!</p>
+
+          <p className="section-p">All meetings with us are via Zoom, be on the lookout for the link in your confirmation email.</p>
+
+          <p className="section-p">If you need assistance, please reach out to our Onboarding Manager at <strong><Link href='mailto:charmaine@stepuptutoring.org'>charmaine@stepuptutoring.org.</Link></strong></p>
+          <div className="center-embed-iframe">
+            <iframe id="my-deferred-iframe" title="Schedule Appointment" width="90%" height="600" frameBorder="0" src={`https://app.acuityscheduling.com/schedule.php?owner=21394641&appointmentType=18777910&firstName=${this.props.tutorDetails.firstname}&lastName=${this.props.tutorDetails.lastname}&email=${this.props.tutorDetails.email}`} />
           </div>
         </div>
       )
