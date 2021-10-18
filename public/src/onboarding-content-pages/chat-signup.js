803a2477654ff1f9922c56c95406f63e115844f5
Eric Castillo <eric.n.castillo@gmail.com>
2021-10-17 17:39:15 -0700

    Added some text per Daniel's recommendation

--- public/src/onboarding-content-pages/chat-signup.js
+++ public/src/onboarding-content-pages/chat-signup.js
@@ -30,6 +30,7 @@ class ChatSignup extends React.Component {
               <div className="content-completed-container">
                 <div className="appointment-confirmation-icon"><CalendarOutlined /></div>
                 <p>{tutorDetails.firstname}, we have you confirmed for your appointment!</p>
+		<p>All the information you need for this meeting should be arriving in your inbox shortly.</p>
                 <p className="appointment-confirmation-title">20 Minute Zoom Interview</p>
                 <p><strong>{new Date(interviewDate).toLocaleString()}</strong> </p>
                 <div>
